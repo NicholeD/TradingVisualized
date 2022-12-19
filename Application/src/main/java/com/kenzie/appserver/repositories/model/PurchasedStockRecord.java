434fc6b5ccde7b07287d81afb11b2f4bbd7292cf
NicholeD <nicholedavid@gmail.com>
2022-12-19 11:02:08 -0700

    Updated PurchasedStockService to create and save fishRecord to FishRepository

--- Application/src/main/java/com/kenzie/appserver/repositories/model/PurchasedStockRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/PurchasedStockRecord.java
@@ -6,13 +6,24 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 @DynamoDBTable(tableName = "purchased")
 public class PurchasedStockRecord {
+
+    private String userId;
     private String name;
     private String symbol;
     private String dateOfPurchase;
     private Double purchasePrice;
     private int shares;
 
-    @DynamoDBHashKey(attributeName = "Name")
+    @DynamoDBHashKey(attributeName = "UserId")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "Name")
     public String getName() {
         return name;
     }
@@ -20,6 +31,7 @@ public class PurchasedStockRecord {
     public void setName(String name) {
         this.name = name;
     }
+
     @DynamoDBAttribute(attributeName = "Symbol")
     public String getSymbol() {
         return symbol;
--- Application/src/main/java/com/kenzie/appserver/service/PurchasedStockService.java
+++ Application/src/main/java/com/kenzie/appserver/service/PurchasedStockService.java
@@ -1,47 +1,54 @@
 package com.kenzie.appserver.service;
 
+import com.amazonaws.services.ec2.model.Purchase;
+import com.kenzie.appserver.controller.model.PurchaseStockRequest;
+import com.kenzie.appserver.repositories.FishRepository;
 import com.kenzie.appserver.repositories.PurchasedStockRepository;
+import com.kenzie.appserver.repositories.model.FishRecord;
 import com.kenzie.appserver.repositories.model.PurchasedStockRecord;
 
+import com.kenzie.appserver.service.model.PurchasedStock;
+import com.kenzie.appserver.service.model.Stock;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PurchasedStockService {
     private PurchasedStockRepository purchasedStockRepository;
+    private FishRepository fishRepository;
 
-    public PurchasedStockService(PurchasedStockRepository purchasedStockRepository) {
+    public PurchasedStockService(PurchasedStockRepository purchasedStockRepository,
+                                 FishRepository fishRepository) {
         this.purchasedStockRepository = purchasedStockRepository;
+        this.fishRepository = fishRepository;
         }
 
-//    public PurchasedStock purchaseStock(PurchaseStockRequest purchaseStockRequest) { //TODO - "String" needs to be "Stock", waiting for Stock model to be created
-//        if (purchaseStockRequest.getShares() <=0) {
-//            //throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Qty has to be greater than 0, one simply cannot purchase nothing")
-//        }
+    public PurchasedStock purchaseStock(PurchaseStockRequest purchaseStockRequest) {
+        if (purchaseStockRequest.getShares() <=0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Qty has to be greater than 0, one simply cannot purchase nothing");
+        }
 
-    // if stock symbol is invalid throw new IllegalArgumentException (
-//
-//        PurchasedStockRecord purchasedStockRecord = new PurchasedStockRecord();
-//        //TODO waiting on Stock model class to implement this:
-////        purchasedStockRecord.setName(purchaseStockRequest.getName());  TODO - cached list of key-value map?? key = symbol, value = name??
-////        purchasedStockRecord.setSymbol(purchaseStockRequest.getSymbol());
-//        purchasedStockRecord.setDateOfPurchase(LocalDate.now().toString()); //symantics are wrong here
-////        purchasedStockRecord.setPurchasePrice(purchaseStockRequest.getPurchasePrice());
-////        purchasedStockRecord.setShares(purchaseStockRequest.getShares());
-//
-//        purchasedStockRepository.save(purchasedStockRecord);
-//
-//        return new PurchasedStock(purchasedStockRecord.getName(),
-//        purchasedStockRecord.getSymbol(),
-//        purchasedStockRecord.getDateOfPurchase(),
-//        purchasedStockRecord.getPurchasePrice(),
-//        purchasedStockRecord.getShares());
-//        }
-//
-//
-//
+        PurchasedStockRecord record = purchaseRequestToRecord(purchaseStockRequest);
+        purchasedStockRepository.save(record);
+
+        FishRecord fishRecord= new FishRecord();
+        fishRecord.setName(record.getName());
+        fishRecord.setPrice(record.getPurchasePrice());
+        fishRecord.setQuantity(record.getShares());
+        fishRepository.save(fishRecord);
+
+        PurchasedStock purchasedStock = recordToStock(record);
+
+        return purchasedStock;
+    }
+
+
 // why did i write this method in this service class again?? wouldn't this be under a portfolio service class??
     public List<PurchasedStock> findByStockSymbol(String symbol) {
             List<PurchasedStockRecord> purchasedStockRecords = purchasedStockRepository
@@ -50,11 +57,30 @@ public class PurchasedStockService {
             List<PurchasedStock> purchasedStock = new ArrayList<>();
 
             for (PurchasedStockRecord record : purchasedStockRecords) {
-                purchasedStock.add(new PurchasedStock(record.getName(), record.getSymbol(),
-        record.getDateOfPurchase(), record.getPurchasePrice(), record.getShares()));
+                purchasedStock.add(recordToStock(record));
         }
 
             return purchasedStock;
 
         }
+
+    private PurchasedStockRecord purchaseRequestToRecord(PurchaseStockRequest request) {
+        PurchasedStockRecord purchasedStockRecord = new PurchasedStockRecord();
+        purchasedStockRecord.setUserId(request.getUserId());
+        purchasedStockRecord.setName(request.getStockName());  //TODO - cached list of key-value map?? key = symbol, value = name??
+        purchasedStockRecord.setSymbol(request.getStockSymbol());
+        purchasedStockRecord.setDateOfPurchase(LocalDate.now().toString());
+        purchasedStockRecord.setPurchasePrice(request.getPurchasePrice());
+        purchasedStockRecord.setShares(request.getShares());
+        purchasedStockRepository.save(purchasedStockRecord);
+
+        return purchasedStockRecord;
+    }
+
+    private PurchasedStock recordToStock(PurchasedStockRecord record) {
+        Stock stock = new Stock(record.getSymbol(), record.getName(), record.getPurchasePrice(),
+                record.getShares(), record.getDateOfPurchase());
+
+        return new PurchasedStock(record.getUserId(), stock, stock.getPurchaseDate());
+    }
  }
