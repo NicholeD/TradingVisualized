0cb350dfad5a1d0fd80e5d57dd2040b5f63c6c22
AJ <ajbennett92498@gmail.com>
2022-12-19 15:41:48 -0500

    PurchasedStockService.java is just commented please feel free to overwrite

--- Application/src/main/java/com/kenzie/appserver/service/model/converter/ConversionTester.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/converter/ConversionTester.java
@@ -1,15 +1,16 @@
 package com.kenzie.appserver.service.model.converter;
 
 import com.kenzie.appserver.service.model.Fish;
+import io.swagger.v3.core.util.Json;
 
 import java.io.File;
 import java.util.List;
 
 public class ConversionTester {
     public static void main(String[] args) {
-        Fish fish = new Fish("Bass", 2.5f, 10, 5.99, true);
-        Fish fish4 = new Fish("Bass", 2.5f, 11, 5.99, true);
-        Fish fish5 = new Fish("Trout", 1.5f, 5, 3.99, false);
+        Fish fish = new Fish("Bass", 2.5f, 10, 5.99, "true");
+        Fish fish4 = new Fish("Bass", 2.5f, 11, 5.99, "true");
+        Fish fish5 = new Fish("Trout", 1.5f, 5, 3.99, "false");
         List<Fish> fishList = List.of(fish, fish4, fish5);
         String json = JsonFishConverter.convertToJson(fishList);
         System.out.println(json);
@@ -18,5 +19,6 @@ public class ConversionTester {
         List<Fish> fish3 = JsonFishConverter.convertToFishFromFile(new File("exe\\TV_Data\\data.txt"));
         System.out.println(fish3);
         System.out.println(JsonFishConverter.convertToJson(fish3));
+        JsonFishConverter.convertToJsonFile(fish3, new File("exe\\TV_Data\\data2.txt"));
     }
 }
