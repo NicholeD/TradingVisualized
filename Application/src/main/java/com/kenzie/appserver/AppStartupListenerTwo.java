43ed31cd412e31758d42e9b1d67a5d8190866b5d
Tanner Stahara <74780833+Tanner-Stahara@users.noreply.github.com>
2022-12-29 00:13:21 -0700

    written some notes for editing / proofing later.

--- Application/src/main/java/com/kenzie/appserver/AppStartupListenerTwo.java
+++ Application/src/main/java/com/kenzie/appserver/AppStartupListenerTwo.java
@@ -5,6 +5,12 @@ import java.util.EventListener;
 import java.util.EventObject;
 import java.util.List;
 
+
+/* NICHOLE - DISREGARD THESE 2 CLASSES, I WAS COMPARING THEM ALONGSIDE SOME OTHER IDEAS I HAD
+THESE ARE ESSENTIALLY DUMMY CLASSES WHILE I PLAY WITH THE POSSIBILITIES FOR HOW I WANT TO TRY AND INTEGRATE ALL OF THIS.
+PLEASE LET ME KNOW WHAT DIRECTIONS YOU ADVISE OR WHERE YOU THINK I SHOULD BE LOOKING TO PROPERLY BUILD THIS!
+ */
+
 class StartupListener implements EventListener {
     public void handleStartupEvent(EventObject event) {
         // Read the contents of the text file
